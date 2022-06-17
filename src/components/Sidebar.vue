9e600b754c926a14dcd6d052d6bbc399acf9673d
aljaz90 <aljaz1.kern@gmail.com>
2022-06-17 18:10:40 +0200

    Add basic project structure

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import AlertIcon from 'vue-ionicons/dist/ios-home.vue'
+</script>
+<template>
+  <div class="sidebar">
+      <div class="sidebar--section sidebar--section-header">
+          <div class="sidebar--section--item sidebar--section--item-title">
+            Events7
+          </div>
+      </div>
+      <div class="sidebar--section">
+          <div class="sidebar--section--item sidebar--section--item-link sidebar--section--item-link-selected">
+            <AlertIcon />
+            Dashboard
+          </div>
+          <div class="sidebar--section--item sidebar--section--item-link">
+            <AlertIcon />
+            Dashboard
+          </div>
+          <div class="sidebar--section--item sidebar--section--item-link">
+            <AlertIcon />
+            Dashboard
+          </div>
+      </div>
+      <div class="sidebar--section">
+      </div>
+  </div>
+</template>
---
+++ src/containers/Layout.vue
@@ -0,0 +1,8 @@
+<script setup>
+    import Navigation from '../components/Navigation.vue';
+    import Sidebar from '../components/Sidebar.vue';
+</script>
+<template>
+    <Navigation />
+    <Sidebar />
+</template>
