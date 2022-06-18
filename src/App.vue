9b63ed0277152ba361273b76eefa43ccfdeab0ce
aljaz90 <aljaz1.kern@gmail.com>
2022-06-18 11:54:30 +0200

    Start work on tracker popup

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup>
-  import Layout from './containers/Layout.vue';
-  import EventCreation from './components/EventCreation.vue';
-  import EventList from './components/EventList.vue';
-  import EventSummary from './components/EventSummary.vue';
+    import Layout from './containers/Layout.vue';
+    import EventCreation from './components/EventCreation.vue';
+    import EventList from './components/EventList.vue';
+    import EventSummary from './components/EventSummary.vue';
+
+    const trackers = [
+        { id: 1, type: "app", name: "click-event", description: "when the user clicks the button the event should be triggered", priority: 0, relatedEvents: [1, 2] },
+        { id: 1, type: "app", name: "click-event", description: "when the user clicks the button the event should be triggered", priority: 0, relatedEvents: [1, 2] },
+        { id: 1, type: "app", name: "click-event", description: "when the user clicks the button the event should be triggered", priority: 0, relatedEvents: [1, 2] }
+    ];
 </script>
 
 <template>
-  <Layout />
-  <EventSummary />
-  <EventCreation />
-  <EventList />
+    <Layout />
+    <EventSummary />
+    <EventCreation />
+    <EventList :trackers="trackers" />
 </template>
--- src/components/EventList.vue
+++ src/components/EventList.vue
@@ -1,15 +1,67 @@
 <script setup>
+    import { ref } from 'vue'
+
+    const props = defineProps({
+        trackers: {
+            type: Array,
+            required: true
+        }
+    });
+
+    const selectedTracker = ref(null);
+
+    const selectTracker = id => {
+        let tracker = props.trackers.find(el => el.id == id);
+        selectedTracker.value = tracker;
+    };
+    const unselectTracker = () => {
+        selectedTracker.value = null;
+    };
+    const handlePopupClick = e => {
+        e.preventDefault();
+        e.stopPropagation();
+    };
+
+
 </script>
 <template>
+    <div class="tracker_popup" @click="unselectTracker" v-if="selectedTracker">
+        <div class="tracker_popup--popup" @click="handlePopupClick" v-if="selectedTracker">
+            
+        </div>
+    </div>
     <div class="dashboard--event_list">
         <div class="dashboard--event_list--title">
             Event trackers
         </div>
         <div class="dashboard--event_list--list">
-            <div class="dashboard--event_list--list--item">
-                <span class="dashboard--event_list--list--item--value">
-
+            <div v-for="tracker in trackers" @click="selectTracker(tracker.id)" class="dashboard--event_list--list--item">
+                <span class="dashboard--event_list--list--item--group">
+                    <div class="dashboard--event_list--list--item--id">
+                        <span class="dashboard--event_list--list--item--id--label">
+                            Id
+                        </span>
+                        {{ tracker.id }}
+                    </div>
+                    <div class="dashboard--event_list--list--item--type">
+                        <span class="dashboard--event_list--list--item--type--label">
+                            Type
+                        </span>
+                        {{ tracker.type }}
+                    </div>
+                    <div class="dashboard--event_list--list--item--name">
+                        <span class="dashboard--event_list--list--item--name--label">
+                            Name
+                        </span>
+                        {{ tracker.name }}
+                    </div>
                 </span>
+                <div class="dashboard--event_list--list--item--priority">
+                    <span class="dashboard--event_list--list--item--priority--label">
+                        Priority
+                    </span>
+                    {{ tracker.priority }}
+                </div>
             </div>
         </div>
         <div class="btn dashboard--event_list--view_all">
